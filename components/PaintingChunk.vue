0c4a7da37ad068c371ef58756c10197f4cbeba05
Cyrus Scott Cowley <cyficowley@fb.com>
2022-04-17 23:10:17 -0700

    all images fo real

--- components/PaintingChunk.vue
+++ components/PaintingChunk.vue
@@ -34,7 +34,7 @@ export default {
   },
   data() {
     return {
-      revealed: '',
+      revealed: false,
     }
   },
   methods: {
--- static/convertToWebp.py
+++ static/convertToWebp.py
@@ -5,6 +5,8 @@ imageNames = [each.split(".")[0] for each in images]
 
 cwd = os.getcwd()
 for (index, imageName) in enumerate(imageNames):
+    if(images[index].endswith("webp")):
+        continue
     os.system(
-        f"~/Downloads/libwebp-0.4.1-mac-10.8/bin/cwebp -q 80 {cwd}/paintings/{images[index]} -o {cwd}/paintingswebp/{imageName}.webp"
+        f"~/Downloads/libwebp-0.4.1-mac-10.8/bin/cwebp -q 80 {cwd}/paintings/{images[index]} -o {cwd}/paintings/{imageName}.webp"
     )
