9fb295b1ab95768a1d0584fda72cc8b8da682365
Cyrus Scott Cowley <cyficowley@fb.com>
2022-06-08 00:05:47 -0700

    Adding 3 years of paintings

--- static/get_paintings/get_imgs.py
+++ static/get_paintings/get_imgs.py
@@ -3,49 +3,45 @@ import pandas
 import os
 import json
 
-painting_data = pandas.read_csv('fitlered_paintings.csv')
+painting_data = pandas.read_csv("fitlered_paintings.csv")
 
 json_stuff = []
 for index, row in painting_data.iterrows():
-    
-    # TODO: remove this part when we want to run all of them
-    if index > 2:
-        break
 
-    img_url = row['img_url']
-    file_name = './retrieved_paintings/' + img_url.split('/')[-1]
+    # # TODO: remove this part when we want to run all of them
+    # if index > 2:
+    #     break
+
+    img_url = row["img_url"]
+    file_name = "./retrieved_paintings/" + img_url.split("/")[-1]
 
     response = requests.get(img_url, stream=True)
 
     if not response.ok:
-        print('removed:', file_name, response)
+        print("removed:", file_name, response)
         continue
 
-    path = '/paintings/' + img_url.split('/')[-1]
-    path = path.replace('jpg', 'webp')
-    artist = row['painting_artist']
-    name = row['painting_name']
+    path = "/paintings/" + img_url.split("/")[-1]
+    path = path.replace("jpg", "webp")
+    artist = row["painting_artist"]
+    name = row["painting_name"]
 
-    json_stuff.append({
-        'artist': artist,
-        'name': name,
-        'path': path
-    })
+    json_stuff.append({"artist": artist, "name": name, "path": path})
 
-    with open(file_name, 'wb') as opened_file:
+    with open(file_name, "wb") as opened_file:
         for block in response.iter_content(1024):
             if not block:
                 break
             opened_file.write(block)
 
-images = os.listdir('./retrieved_paintings')
+images = os.listdir("./retrieved_paintings")
 
 # check size of all images
-print('Checking size of all images...')
+print("Checking size of all images...")
 for image in images:
-    file_size = os.path.getsize('./retrieved_paintings/' + image)
-    assert(file_size > 0)
-print('All images are valid.')
+    file_size = os.path.getsize("./retrieved_paintings/" + image)
+    assert file_size > 0
+print("All images are valid.")
 
 # json_stuff = []
 # for index, row in painting_data.iterrows():
@@ -64,6 +60,6 @@ print('All images are valid.')
 #     })
 
 # write to JSON file
-json_dumb = {'paintings': json_stuff}
-with open('ignore.json', 'w') as outfile:
+json_dumb = {"paintings": json_stuff}
+with open("ignore.json", "w") as outfile:
     json.dump(json_dumb, outfile)
