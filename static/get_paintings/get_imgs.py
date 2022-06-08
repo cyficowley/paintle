4f4384ced829d4ff86128a0a9d7ca14e688e9578
Darya Verzhbinsky <darya99@gmail.com>
2022-06-07 18:19:13 -0700

    updated vue format

--- static/get_paintings/get_imgs.py
+++ static/get_paintings/get_imgs.py
@@ -1,12 +1,16 @@
 import requests
 import pandas
 import os
+import json
 
 painting_data = pandas.read_csv('fitlered_paintings.csv')
 
 for index, row in painting_data.iterrows():
+
+    # TODO: remove this part when we want to run all of them
     if index > 20:
         break
+
     img_url = row['img_url']
     file_name = './retrieved_paintings/' + img_url.split('/')[-1]
 
@@ -14,7 +18,6 @@ for index, row in painting_data.iterrows():
 
     if not response.ok:
         print('removed:', file_name, response)
-        # os.remove(file_name)
         continue
 
     with open(file_name, 'wb') as opened_file:
@@ -31,3 +34,24 @@ for image in images:
     file_size = os.path.getsize('./retrieved_paintings/' + image)
     assert(file_size > 0)
 print('All images are valid.')
+
+json_stuff = []
+for index, row in painting_data.iterrows():
+    # ,img_url,painting_artist,painting_name
+    img_url = row['img_url']
+
+    path = '/paintings/' + img_url.split('/')[-1]
+    path = path.replace('jpg', 'webp')
+    artist = row['painting_artist']
+    name = row['painting_name']
+
+    json_stuff.append({
+        'artist': artist,
+        'name': name,
+        'path': path
+    })
+
+# write to JSON file
+json_dumb = {'paintings': json_stuff}
+with open('ignore.json', 'w') as outfile:
+    json.dump(json_dumb, outfile)
